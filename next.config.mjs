5ba3d9442e30b1b468b91205f5239c2660e6a730
Rahul Kushwaha <rk.rahulkushwaha22@gmail.com>
2025-12-14 18:02:26 +0530

    chore: update next.config.mjs for GitHub Pages export and basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = 'pleasure-energy-drink';
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -6,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
 }
 
 export default nextConfig
