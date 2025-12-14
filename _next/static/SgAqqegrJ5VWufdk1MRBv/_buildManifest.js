c518f7c44caf0a4b1d35de65bf4e5fef6c1dc293
the-rahulk <the-rahulk@users.noreply.github.com>
2025-12-14 12:53:15 +0000

    deploy: 3b7a3a2301c3c1945498958f75f729ec8bffe89d

---
+++ _next/static/SgAqqegrJ5VWufdk1MRBv/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/pleasure-energy-drink//_next/:path+",
+        "destination": "/pleasure-energy-drink/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
